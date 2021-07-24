289c59c99759f3ced33de264dcf726c7ba88169e
utgwkk <utagawakiki@gmail.com>
2021-07-24 15:46:55 +0900

    nginxの設定を管理する

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,12 @@ make
 sudo systemctl restart isu-go
 cd -
 
+# deploy nginx config
+sudo cp webapp/etc/nginx/nginx.conf /etc/nginx/nginx.conf
+sudo cp webapp/etc/nginx/sites-available/isucon.conf /etc/nginx/sites-available/isucon.conf
+sudo nginx -t
+sudo systemctl reload nginx
+
 # deploy mysql config
 sudo cp webapp/etc/mysql/conf.d/my.cnf /etc/mysql/conf.d/my.cnf
 sudo systemctl restart mysql
